ff0f38aac610fc691125e52d14e33a875543ac9c
Kundan-Rawal <rawalkundan987@gmail.com>
2025-07-25 02:10:47 +0530

    new values added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import express from "express"
 import pool from "./database/db.js";
+import dotenv from 'dotenv';
 import path from "path"
 
 const app=express();
 
+app.listen(process.env.PORT,()=>{
+    console.log("server Running On Port 3000")
+})
+
+//to check api fetch
 app.get("/",(req,res)=>{
     res.send("Hello World!");
 });
 
-app.listen(3000,()=>{
-    console.log("server Running On Port 3000")
-})
+
+app.get("/customers", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM customers;");
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: "Database error", details: err.message });
+    }
+});
+
+
