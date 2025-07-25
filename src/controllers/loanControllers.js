2a6b706dfb90591905ba4b85f839421f297b3fd1
Kundan-Rawal <rawalkundan987@gmail.com>
2025-07-25 15:07:27 +0530

    2nd API Endpoint tested in localhost

--- src/controllers/loanControllers.js
+++ src/controllers/loanControllers.js
@@ -23,3 +23,48 @@ export const createLoan = (req,res)=>{
                 res.status(500).json({ error: "Database error", details: e.message });
             }
 }
+
+
+export const recordPayment = async (req, res) => {
+    const {loan_id} = req.params;
+    const {amount,payment_type} = req.body;
+    const payment_id = uuidv4();
+    const payment_date = new Date().toISOString();
+
+    try{
+    const loanResult = await pool.query(`SELECT * FROM loans WHERE loan_id = $1`, [loan_id]);
+    if(loanResult.rows.length === 0) {
+        return res.status(404).json({ error: "Loan not found" });
+    }
+
+    if (!amount || !payment_type || !["EMI", "LUMP_SUM"].includes(payment_type)) {
+    return res.status(400).json({ error: "Invalid payment data" });
+    }
+
+
+    pool.query(`INSERT INTO payments (payment_id,loan_id,amount,payment_type,payment_date) values ($1,$2,$3,$4,$5)`,[payment_id,loan_id,amount,payment_type,payment_date])
+
+    const loan = loanResult.rows[0];
+    const previousPayments = await pool.query(`SELECT COALESCE(SUM(amount), 0) as total_paid FROM payments WHERE loan_id = $1`, [loan_id]);
+    const totalPaid = parseFloat(previousPayments.rows[0].total_paid);
+    const remainingBalance = parseFloat(loan.total_amount) - totalPaid - parseFloat(amount);
+        
+    const emis_left = Math.ceil(remainingBalance / parseFloat(loan.monthly_emi));
+
+    if (remainingBalance <= 0) {
+      await pool.query(`UPDATE loans SET status = 'PAID_OFF' WHERE loan_id = $1`, [loan_id]);
+    }
+
+    res.status(200).json({
+      payment_id,
+      loan_id,
+      message: "Payment recorded successfully.",
+      remaining_balance: Math.max(0, Number(remainingBalance.toFixed(2))),
+      emis_left: remainingBalance > 0 ? emis_left : 0,
+    });
+
+    }catch(e){
+        console.error("Error recording payment:", e);
+        return res.status(500).json({ error: "Database error", details: e.message });
+    }
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-import {createLoan} from "../controllers/loanControllers.js";
+import {createLoan,recordPayment} from "../controllers/loanControllers.js";
 import express from "express";
 import dotenv from 'dotenv';
 const router = express.Router();
@@ -6,5 +6,7 @@ const router = express.Router();
 const base_url = `/api/v1`;
 
 router.post(`${base_url}/loans`, createLoan);
+
+router.post(`${base_url}/loans/:loan_id/payments`, recordPayment)
 
 export default router;
